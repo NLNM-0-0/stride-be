d97be916a4f4a306f2ddbdaadc113c39ec36eeba
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-13 10:43:25 +0700

    dev: use cached category when fetching sport

--- core-service/src/main/java/com/stride/tracking/coreservice/service/impl/SportCacheService.java
+++ core-service/src/main/java/com/stride/tracking/coreservice/service/impl/SportCacheService.java
@@ -3,6 +3,7 @@ package com.stride.tracking.coreservice.service.impl;
 import com.stride.tracking.commons.exception.StrideException;
 import com.stride.tracking.coreservice.constant.CacheName;
 import com.stride.tracking.coreservice.constant.Message;
+import com.stride.tracking.coreservice.model.Category;
 import com.stride.tracking.coreservice.model.Sport;
 import com.stride.tracking.coreservice.repository.SportRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SportCacheService {
     private final SportRepository sportRepository;
+    private final CategoryCacheService categoryCacheService;
 
     @Cacheable(value = CacheName.SPORT_BY_ID, key = "#sportId")
     @Transactional(readOnly = true)
     public Sport findSportById(String sportId) {
-        return sportRepository.findById(sportId).orElseThrow(
+        Sport sport = sportRepository.findById(sportId).orElseThrow(
                 () -> new StrideException(HttpStatus.BAD_REQUEST, Message.SPORT_NOT_FOUND)
         );
+
+        Category category = categoryCacheService.findById(sport.getCategory().getId());
+        sport.setCategory(category);
+
+        return sport;
     }
 }
