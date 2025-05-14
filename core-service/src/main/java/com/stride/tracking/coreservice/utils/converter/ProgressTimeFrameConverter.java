4dc40519be00526d14aca6e4410be18d109dd18c
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2025-05-14 10:54:00 +0700

    dev: use converter for ProgressTimeFrame

---
+++ core-service/src/main/java/com/stride/tracking/coreservice/utils/converter/ProgressTimeFrameConverter.java
@@ -0,0 +1,19 @@
+package com.stride.tracking.coreservice.utils.converter;
+
+import com.stride.tracking.coreservice.constant.ProgressTimeFrame;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProgressTimeFrameConverter implements Converter<String, ProgressTimeFrame> {
+    @Override
+    public ProgressTimeFrame convert(@NonNull String source) {
+        for (ProgressTimeFrame tf : ProgressTimeFrame.values()) {
+            if (tf.getName().equalsIgnoreCase(source)) {
+                return tf;
+            }
+        }
+        throw new IllegalArgumentException("Unknown time frame: " + source);
+    }
+}
